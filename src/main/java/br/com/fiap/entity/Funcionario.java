236d049a36a7b31867b3cb9885c902d4e14c63bd
SunaUezuri <wesley.jane.santos@gmail.com>
2025-03-21 18:02:04 -0300

    Mapeando as classes com anotações

--- src/main/java/br/com/fiap/entity/Funcionario.java
+++ src/main/java/br/com/fiap/entity/Funcionario.java
@@ -1,9 +1,32 @@
 package br.com.fiap.entity;
 
+import br.com.fiap.annotations.Coluna;
+import br.com.fiap.annotations.Tabela;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity
+@Tabela(nome = "TAB_FUNCIONARIO")
+@Table(name = "TAB_FUNCIONARIO")
+@SequenceGenerator(name = "funcionario", sequenceName = "SQ_TAB_FUNCIONARIO", allocationSize = 1)
 public class Funcionario {
 
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "funcionario")
+    @Coluna(nome = "id")
+    private Long id;
+
+    @Coluna(nome = "nome", obrigatorio = true, tamanho = 100)
+    @Column(nullable = false, length = 100)
     private String nome;
+
+    @Coluna(nome = "salario", obrigatorio = true)
+    @Column(nullable = false)
     private double salario;
+
+    @Coluna(nome = "hr_trabalhadas", obrigatorio = true)
+    @Column(name = "hr_trabalhadas", nullable = false)
     private int horasTrabalhadas;
 
     @Override
--- src/main/java/br/com/fiap/entity/Gerente.java
+++ src/main/java/br/com/fiap/entity/Gerente.java
@@ -1,9 +1,19 @@
 package br.com.fiap.entity;
 
+import br.com.fiap.annotations.Coluna;
+import br.com.fiap.annotations.Tabela;
 import br.com.fiap.entity.enums.Setor;
 
+import javax.persistence.*;
+
+@Entity
+@Tabela(nome = "TAB_GERENTE")
+@Table(name = "TAB_GERENTE")
 public class Gerente extends Funcionario{
 
+    @Enumerated(EnumType.STRING)
+    @Coluna(nome = "setor", obrigatorio = true)
+    @Column(nullable = false)
     private Setor setor;
 
     public Gerente(String nome, double salario, int horasTrabalhadas, Setor setor) {
