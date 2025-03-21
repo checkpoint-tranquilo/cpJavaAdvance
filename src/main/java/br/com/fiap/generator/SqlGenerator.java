5992adba366fcdf2f824e41c6755668952b668e1
SunaUezuri <wesley.jane.santos@gmail.com>
2025-03-21 18:24:55 -0300

    Criando um gerador de código SQL

---
+++ src/main/java/br/com/fiap/generator/SqlGenerator.java
@@ -0,0 +1,101 @@
+package br.com.fiap.generator;
+
+import br.com.fiap.annotations.Coluna;
+import br.com.fiap.annotations.Tabela;
+
+import java.lang.reflect.Field;
+
+public class SqlGenerator {
+
+    public static String gerarSelect(Class<?> clazz) {
+        if (!clazz.isAnnotationPresent(Tabela.class)) {
+            throw new IllegalArgumentException("A classe não está anotada com @Tabela");
+        }
+
+        Tabela tabela = clazz.getAnnotation(Tabela.class);
+        return "SELECT * FROM " + tabela.nome() + ";";
+    }
+
+    public static String gerarInsert(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+
+        if (!clazz.isAnnotationPresent(Tabela.class)) {
+            throw new IllegalArgumentException("A classe não está anotada com @Tabela");
+        }
+
+        Tabela tabela = clazz.getAnnotation(Tabela.class);
+        StringBuilder campos = new StringBuilder();
+        StringBuilder valores = new StringBuilder();
+
+        for (Field f : clazz.getDeclaredFields()) {
+            f.setAccessible(true);
+            if (f.isAnnotationPresent(Coluna.class)) {
+                Coluna coluna = f.getAnnotation(Coluna.class);
+                campos.append(coluna.nome()).append(", ");
+                valores.append("'").append(f.get(obj)).append("', ");
+            }
+        }
+
+        //Remover a última vírgula
+        if (campos.length() > 2) {
+            campos.setLength(campos.length() - 2);
+            valores.setLength(valores.length() - 2);
+        }
+        return "INSERT INTO " + tabela.nome() + " (" + campos + ") VALUES (" + valores + ");";
+    }
+
+    public static String gerarUpdate(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+
+        if (!clazz.isAnnotationPresent(Tabela.class)) {
+            throw new IllegalArgumentException("A classe não está anotada com @Tabela");
+        }
+
+        Tabela tabela = clazz.getAnnotation(Tabela.class);
+        StringBuilder setClause = new StringBuilder();
+        String idColuna = null;
+        String idValor = null;
+
+        for (Field f : clazz.getDeclaredFields()) {
+            f.setAccessible(true);
+            if (f.isAnnotationPresent(Coluna.class)) {
+                Coluna coluna = f.getAnnotation(Coluna.class);
+                if (f.getName().equalsIgnoreCase("id")) {
+                    idColuna = coluna.nome();
+                    idValor = f.get(obj).toString();
+                } else {
+                    setClause.append(coluna.nome()).append(" = '").append(f.get(obj)).append("', ");
+                }
+            }
+        }
+
+        //Remover a última vírgula
+        if (setClause.length() > 2) {
+            setClause.setLength(setClause.length() - 2);
+        }
+        return "UPDATE " + tabela.nome() + " SET " + setClause + " WHERE " + idColuna + " = " + idValor + ";";
+    }
+
+    public static String gerarDelete(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+
+        if (!clazz.isAnnotationPresent(Tabela.class)) {
+            throw new IllegalArgumentException("A classe não está anotada com @Tabela");
+        }
+
+        Tabela tabela = clazz.getAnnotation(Tabela.class);
+        String idColuna = null;
+        String idValor = null;
+
+        for (Field f : clazz.getDeclaredFields()) {
+            f.setAccessible(true);
+            if (f.isAnnotationPresent(Coluna.class) && f.getName().equalsIgnoreCase("id")) {
+                idColuna = f.getAnnotation(Coluna.class).nome();
+                idValor = f.get(obj).toString();
+                break;
+            }
+        }
+
+        return "DELETE FROM " + tabela.nome() + " WHERE " + idColuna + " = " + idValor + ";";
+    }
+}
